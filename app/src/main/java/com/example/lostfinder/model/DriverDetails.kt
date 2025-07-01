4a46be68e328c0fcde33c823d9004ae0baa634b2
Ataussamad Ansari <sam.an.vns6@gmail.com>
2025-07-01 15:16:46 +0530

    add and update model class

--- app/src/main/java/com/example/lostfinder/model/DriverDetails.kt
+++ app/src/main/java/com/example/lostfinder/model/DriverDetails.kt
@@ -2,18 +2,17 @@ package com.example.lostfinder.model
 
 data class DriverDetails(
     val name: String,
-    val email: String?,                       // Optional
+    val email: String?,
     val phoneNumber: String,
     val address: String,
+
     val licenseNumber: String,               // Driver License (DL)
     val vehicleNumber: String,               // e.g., MH12AB1234
     val vehicleType: String,                 // Car, Bike, Truck
     val vehicleModel: String?,               // e.g., Swift Dzire
-    val vehicleColor: String?,               // e.g., White, Red
     val profileImageUrl: String?,            // Driver photo URL
-    val rating: Float?,                      // Optional rating
-    val experienceYears: Int?,               // Optional experience
     val verified: Boolean = false,           // Is verified by admin?
+    val verifiedMsg: String?,
 
     // KYC Details
     val kycType: KycType,                    // Enum: AADHAAR, PAN
@@ -21,5 +20,3 @@ data class DriverDetails(
     val kycFrontImageUrl: String?,           // Image of Aadhar/PAN front
     val kycBackImageUrl: String?,            // Only if Aadhar has back image
 )
-
-
---
+++ app/src/main/java/com/example/lostfinder/model/RiderDetails.kt
@@ -0,0 +1,11 @@
+package com.example.lostfinder.model
+
+data class RiderDetails(
+    val id: String? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val phoneNumber: String,
+    val profileImageUrl: String? = null,
+    val gender: String? = null,
+    val address: String? = null,
+)
